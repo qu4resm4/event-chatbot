8db1da76f31410eb72b4a1283016f6abc16bf5b4
qu4resm4 <quaresmacont@gmail.com>
2025-02-21 20:55:31 -0300

    feat: método para vincular arquivo ao assistente

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
+import fs from "fs";
+import path from "path";
 
 @Injectable()
 export class OpenaiService {
 
+    private openai: OpenAI;
     private teste; 
     private assistantID;
     private instrucoes = `Você é um assistente especializado em formatação de dados para IA. Sua tarefa é estruturar informações de palestras em um JSON multilíngue padronizado, incluindo formatação localizada de data e horário.`;
@@ -16,39 +19,79 @@ export class OpenaiService {
 
     async onModuleInit(): Promise<void> {
         try {
-          // Verifica se já existe um assistente no banco// se ele ja existir, nao precisar criar outro, mexer o codigo
-          const assistant = await this.teste // método que acessa o banco de dados
-          if (assistant) {
-            this.assistantID = assistant.id;
+          // verifica 
+          const assistenteDefinidoNoEnv = process.env.ID_ASSISTENT;
+          if(assistenteDefinidoNoEnv !== ''){
+            this.assistantID = assistenteDefinidoNoEnv;
           } else {
-            await this.criarAssistente();
-            console.log('Novo assistente criado com ID:', this.assistantID);
+            // Verifica se já existe um assistente no banco, se não existe cria um
+            const assistant = await this.teste // método que acessa o banco de dados
+            if (assistant) {
+              this.assistantID = assistant.id;
+            } else {
+              await this.criarAssistente();
+              console.log('Novo assistente criado com ID:', this.assistantID);
+            }
           }
         } catch (error) {
           console.error('Erro ao verificar ou criar assistente:', error);
           throw new Error('Erro ao verificar ou criar assistente');
         }
       }
+
+    private async adicionarArquivoNoAssistente(assistenteID){
+      try {
+        // caminho do arquivo
+        const filePath = path.join(__dirname, process.env.PATH_ARQUIVO_PARA_BUSCA);
+
+        // stream para enviar o arquivo para a API (transforma o arquivo em um fluxo de dados para ser transferido)
+        const fileStream = fs.createReadStream(filePath);
+
+        // Upload do arquivo pra API
+        const file = await this.openai.files.create({
+          file: fileStream,
+          purpose: "assistants",
+        });
+
+        // Adiciona o arquivo ao "armazenamento vetorial"
+        const vectorStore = await this.openai.beta.vectorStores.create({
+          name: "Meu Vector Store",
+          file_ids: [file.id],
+        });
+
+        // Vincula vector store ao assistente
+        const assistant = await this.openai.beta.assistants.update(assistenteID, {
+          tool_resources: { file_search: { vector_store_ids: [vectorStore.id] } },
+        });
+
+        return console.log("Bem sucedido")
+      } catch {
+        return console.log("Erro ao vincular arquivo de busca ao assistente")
+      }
+    }
     
     // Função para criar um assistente
     private async criarAssistente() {
         try {
-          const myAssistant = await this.openai.beta.assistants.create({
+          const assistant = await this.openai.beta.assistants.create({
+            name: "Guia de Palestras do Web Summit",
             instructions: this.instrucoes,
-            name: 'Guide Assistant',
-            tools: [{ type: 'code_interpreter' }],
-            model: 'gpt-3.5-turbo', // Modelo da OpenAI
+            tools: [{ type: "file_search" }], // Habilita a ferramenta de busca em arquivos
+            model: "gpt-4o-mini",
           });
     
-          this.assistantId = myAssistant.id;
-          console.log('Novo assistente criado com ID:', this.assistantId);
+          this.assistantID = assistant.id;
+          console.log('Novo assistente criado com ID:', this.assistantID);
     
-          // Salvar o ID do assistente no banco de dados
-          await this.prisma.assistant.create({
+          // Salvar o ID do assistente no banco de dados !!!!!!!!!
+          /*await this.prisma.assistant.create({
             data: {
-              id: this.assistantId,
+              id: this.assistantID,
             },
           });
+          */
+
+          this.adicionarArquivoNoAssistente(this.assistantID);
         } catch (error) {
           console.error('Erro ao criar assistente:', error);
           throw new Error('Erro ao criar assistente');

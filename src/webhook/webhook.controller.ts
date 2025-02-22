9646fe075aac02d5f78d6b4b9cab215cf1503976
qu4resm4 <quaresmacont@gmail.com>
2025-02-21 22:07:59 -0300

    fix(webhook.controller): método para api cloud meta verificar o webhook

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Query } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
 
 @Controller('webhook')
 export class WebhookController {
   constructor(private readonly webhookService: WebhookService) {}
+
+  @Get()
+  async verifyWebhook(
+    @Query('hub.mode') mode: string,
+    @Query('hub.challenge') challenge: string,
+    @Query('hub.verify_token') token: string,
+  ) {
+    const verify_token = process.env.WEBHOOK_VERIFY_TOKEN;
+
+    if (mode === 'subscribe' && token === verify_token) {
+      return challenge; // Retorna o challenge se o token de verificação for válido
+    }
+    return 'Erro na verificação do webhook';
+  }
 
   @Post()
   mensagemRecebida(@Body() mensagemRecebidaDto: any) {
